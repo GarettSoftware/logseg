e20634f39d39486930bc494a9238b8b80ef8dbc1
Garett MacGowan <garettsoftware@gmail.com>
2023-11-08 00:02:39 -0500

    Add back cleanup handler cleanup logic in get_logger

--- logseg/log_setup.py
+++ logseg/log_setup.py
@@ -406,9 +406,8 @@ def get_logger(name: str, queue: Optional[Queue] = None) -> Logger:
         # Redirect stdout of this process to the logger instance
         _redirect_stdout_stderr(logger)
 
-        # Clean up handlers.
+        # Clean up existing handlers on the root logger if they exist.
         for handler in root.handlers:
-            handler.flush()
             handler.close()
             root.removeHandler(handler)
 
